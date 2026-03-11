e274ff00254253409fbc9b73588cf99b5acb325a
Dennis Pallett <dennis@dpitservices.nl>
2026-03-11 22:25:28 +0100

    Use Modidect plugin to create runtime

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module nl.pallett.jsoneditor {
+    requires java.desktop;
+    requires java.prefs;
+    requires java.base;
+
+    requires javafx.controls;
+
+    requires org.fxmisc.richtext;
+    requires org.fxmisc.flowless;
+    requires org.controlsfx.controls;
+    requires reactfx;
+
+    requires org.jspecify;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.yaml;
+
+    requires org.slf4j;
+
+    requires org.snakeyaml.engine.v2;
+    requires org.yaml.snakeyaml;
+
+    exports nl.pallett.jsoneditor;
+}
